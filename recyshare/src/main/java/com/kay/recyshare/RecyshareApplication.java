ab511a484b5b25db0ff60fa52b261e7c153d6d95
Kolade-Amire <stephamire@gmail.com>
2023-08-14 14:15:18 +0100

    init commit

---
+++ recyshare/src/main/java/com/kay/recyshare/RecyshareApplication.java
@@ -0,0 +1,13 @@
+package com.kay.recyshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyshareApplication.class, args);
+	}
+
+}
---
+++ recyshare/src/test/java/com/kay/recyshare/RecyshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kay.recyshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
